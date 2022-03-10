503dc83b5d2c3541dc754c38831a5591652f7dd4
Mark Tripney <marktripney@icloud.com>
2022-03-10 00:39:40 +0000

    Add unstyled draft of blog functionality

--- src/routes/blog/_post.svelte
+++ src/routes/blog/_post.svelte
@@ -1,6 +1,7 @@
 <script>
   export let title;
   export let date;
+  export let categories;
 </script>
 
 
@@ -8,6 +9,20 @@
   <h1 class="post-title">{title}</h1>
   <p>Published: {date}</p>
   <slot/>
+  {#if categories.length}
+    <aside>
+      <h2>Posted in:</h2>
+      <ul>
+        {#each categories as category}
+          <li>
+            <a href="/blog/categories/{category}">
+              {category}
+            </a>
+          </li>
+        {/each}
+      </ul>
+    </aside>
+  {/if}
 </div>
 
 <style>
