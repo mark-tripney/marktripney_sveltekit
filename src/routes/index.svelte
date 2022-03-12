27ebf47f3a683adaf6ead76dd3562589836e9203
Mark Tripney <marktripney@icloud.com>
2022-03-12 17:38:10 +0000

    Add initial Cards, largely unstyled

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,10 +1,11 @@
 <script>
+	import Card from '$lib/components/Card.svelte';
 	import Contact from '$lib/components/Contact.svelte';
 </script>
 
-<section id="intro" class="dotted-bg">
+<section id="intro">
 	<div class="content-wrapper">
-		<hr />
+		<hr class="upper-hr" />
 		<h1 class="main-head">Hi, I'm Mark.</h1>
 		<h2 class="main-sub-head">I'm a front-end developer, designer, and writer.</h2>
 		<div class="square" />
@@ -14,20 +15,56 @@
 			necessitatibus perferendis possimus soluta?
 		</p>
 		<p>
-			<span class="wave">👋</span> Want to get in touch? Great! You can reach me
+			<span class="wave">👋</span> Want to get in touch? Great! There's a contact form
 			<a href="#contact">here</a>. I look forward to hearing from you!
 		</p>
 	</div>
 </section>
 
 <section id="work">
-	<div class="content-wrapper">
-		<h2>Work</h2>
-		<p>These are some examples of recent work.</p>
+	<div class="diagonal-div">
+		<div class="content-wrapper diagonal-wrapper">
+			<h2>Work</h2>
+			<p>These are some examples of recent work.</p>
+			<div class="grid-container">
+				<!-- devsearch -->
+				<Card
+					src="/images/devfinder-dark-mix-crop.png"
+					alt="Screenshot from 'devfinder' app, showing light and dark mode side-by-side."
+					title="devfinder"
+					text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus dolorem commodi odit perferendis error obcaecati officia molestias ex illo neque?"
+					link="https://github-user-search-peach.vercel.app/"
+				/>
+				<!-- Password Generator -->
+				<Card
+					src="/images/password-generator-crop.png"
+					alt="Screenshot from 'Password Generator' app."
+					title="Password Generator"
+					text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus dolorem commodi odit perferendis error obcaecati officia molestias ex illo neque?"
+					link="https://password-generator-eta-two.vercel.app/"
+				/>
+				<!-- todos -->
+				<Card
+					src="/images/todo-react-crop.png"
+					alt="Screenshot from 'todos' app."
+					title="todos"
+					text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus dolorem commodi odit perferendis error obcaecati officia molestias ex illo neque?"
+					link="https://react-todos-marktripney.vercel.app/"
+				/>
+				<!-- Pricing Cards with Toggle -->
+				<Card
+					src="/images/pricing-cards-with-toggle-crop.png"
+					alt="Screenshot from 'Pricing Cards' app."
+					title="Pricing Cards with Toggle"
+					text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus dolorem commodi odit perferendis error obcaecati officia molestias ex illo neque?"
+					link="https://pricing-with-toggle-beige.vercel.app"
+				/>
+			</div>
+		</div>
 	</div>
 </section>
 
-<section id="contact" class="dotted-bg">
+<section id="contact">
 	<div class="content-wrapper">
 		<h2>Contact</h2>
 		<Contact />
@@ -36,7 +73,7 @@
 
 <style>
 	#intro {
-		padding: 3rem 0 3.5rem;
+		padding: 0 0 3.5rem;
 	}
 
 	#work {
@@ -47,12 +84,8 @@
 		padding: 3rem 0 4rem;
 	}
 
-	hr {
-		width: 100px;
-		margin: 0 0 3rem 0;
-		height: 10px;
+	.upper-hr {
 		background-color: coral;
-		border: none;
 	}
 
 	.square {
@@ -108,8 +141,28 @@
 		}
 	}
 
-	#work {
-		background-color: #e5e5f1;
+	/*#work h2,*/
+	/*#work p {*/
+	/*	color: #fff;*/
+	/*}*/
+
+	.diagonal-div {
+		background: linear-gradient(45deg, rgba(245, 223, 77, 1) 0%, rgba(252, 252, 252, 1) 100%);
+		transform: skewY(-2deg);
+		padding: 4rem 0;
+	}
+
+	.diagonal-wrapper {
+		transform: skewY(2deg);
+	}
+
+	@media (min-width: 1000px) {
+		.grid-container {
+			display: grid;
+			grid-template-columns: 1fr 1fr;
+			gap: 2rem;
+			padding-bottom: 2rem;
+		}
 	}
 
 	#contact h2 {
