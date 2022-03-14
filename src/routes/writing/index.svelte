69f46e74da6e74f7009f21db5f1a6a2a775312fb
Mark Tripney <marktripney@icloud.com>
2022-03-14 14:39:56 +0000

    Add cards per Post, with category badges

--- src/routes/writing/index.svelte
+++ src/routes/writing/index.svelte
@@ -14,6 +14,23 @@
 
 <script>
 	export let posts;
+	const categoryStyles = {
+		svelte: {
+			name: 'Svelte',
+			background: 'rgba(223, 76, 37, 1)',
+			color: '#fff'
+		},
+		javascript: {
+			name: 'JavaScript',
+			background: 'rgba(245, 223, 77, 1)',
+			color: '#000'
+		},
+		css: {
+			name: 'CSS',
+			background: '#2c4bdd',
+			color: '#fff'
+		}
+	};
 </script>
 
 <div class="content-wrapper">
@@ -26,15 +43,30 @@
 	<ul>
 		{#each posts as post}
 			<li>
-				<h3>
-					<a href={post.path}>
-						{post.meta.title}
-					</a>
-				</h3>
-				Published {formatDate(post.meta.date)}
+				<a href={post.path}>
+					<h3 class="main-link">{post.meta.title}</h3>
+					Published {formatDate(post.meta.date)}
+					{#if post.meta.categories.length}
+						<aside>
+							<div class="categories">
+								{#each post.meta.categories as category}
+									<div
+										class="category"
+										style="background-color: {categoryStyles[category].background};"
+									>
+										<p style="color: {categoryStyles[category].color};">
+											{categoryStyles[category].name}
+										</p>
+									</div>
+								{/each}
+							</div>
+						</aside>
+					{/if}
+				</a>
 			</li>
 		{/each}
 	</ul>
+	<p class="home-link">&larr; <a href="/" class="non-body-link">Home</a></p>
 </div>
 
 <style>
@@ -46,6 +78,7 @@
 		display: flex;
 		align-items: end;
 		width: 100%;
+		margin-bottom: 2rem;
 	}
 
 	.circle {
@@ -67,17 +100,59 @@
 	ul {
 		list-style: none;
 		padding-left: 0;
+		margin-bottom: 3rem;
 	}
 
 	li {
+		position: relative;
 		margin-bottom: 1rem;
+		padding: 0.5rem 1rem;
+		background-color: #fff;
+		border-radius: 8px;
+		box-shadow: 0.8px 0.8px 2.2px rgba(0, 0, 0, 0.02), 2px 2px 5.3px rgba(0, 0, 0, 0.028),
+			3.8px 3.8px 10px rgba(0, 0, 0, 0.035), 6.7px 6.7px 17.9px rgba(0, 0, 0, 0.042),
+			12.5px 12.5px 33.4px rgba(0, 0, 0, 0.05), 30px 30px 80px rgba(0, 0, 0, 0.07);
+	}
+
+	.main-link::before {
+		content: ' ';
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
 	}
 
 	a {
 		text-decoration: none;
+		font-size: 0.9rem;
+	}
+
+	aside {
+		margin-top: 0.5rem;
 	}
 
 	h3 {
 		font-size: 1.5rem;
+	}
+
+	.category {
+		display: inline-block;
+		padding: 4px 10px;
+		border-radius: 12px;
+		margin-bottom: 0.5rem;
+	}
+
+	.category ~ .category {
+		margin-left: 0.5rem;
+	}
+
+	.category p {
+		font-weight: 600;
+		font-size: 0.8rem;
+	}
+
+	.home-link {
+		margin-bottom: 3rem;
 	}
 </style>
