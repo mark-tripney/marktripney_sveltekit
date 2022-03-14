4560a79b4e3e4369f1e92f0d9ef2a414c4ad8bef
Mark Tripney <marktripney@icloud.com>
2022-03-14 17:31:20 +0000

    Add fade transition on page change

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,16 +1,32 @@
+<script context="module">
+	export const load = ({ url }) => {
+		const currentRoute = url.pathname;
+
+		return {
+			props: {
+				currentRoute
+			}
+		};
+	};
+</script>
+
 <script>
 	import Nav from '$lib/components/Nav.svelte';
 	import Footer from '$lib/components/Footer.svelte';
 	import '$lib/styles/global.css';
+	import { fade } from 'svelte/transition';
+	export let currentRoute;
 </script>
 
 <div class="wrapper">
 	<header>
 		<Nav />
 	</header>
-	<main class="dotted-bg">
-		<slot />
-	</main>
+	{#key currentRoute}
+		<main class="dotted-bg" in:fade={{ duration: 150, delay: 150 }} out:fade={{ duration: 150 }}>
+			<slot />
+		</main>
+	{/key}
 	<Footer />
 </div>
 
