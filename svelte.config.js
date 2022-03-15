2e29c9dc806ea03c6d1dfebff61e8be0536c4b60
Mark Tripney <marktripney@icloud.com>
2022-03-15 14:49:02 +0000

    Tidy up formatting; no change to content

--- svelte.config.js
+++ svelte.config.js
@@ -1,22 +1,22 @@
 // TODO: Consider pre-render as static HTML...
 // import adapter from '@sveltejs/adapter-static';
 import adapter from '@sveltejs/adapter-auto';
-import sveltePreprocess from 'svelte-preprocess'
-import { mdsvex } from 'mdsvex'
+import sveltePreprocess from 'svelte-preprocess';
+import { mdsvex } from 'mdsvex';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  kit: { adapter: adapter() },
-  extensions: ['.svelte', '.md'],
-  preprocess: [
-    sveltePreprocess(),
-    mdsvex({
-      extensions: ['.md'],
-      layout: {
-        writing: 'src/routes/writing/_post.svelte'
-      }
-    })
-  ]
-}
+	kit: { adapter: adapter() },
+	extensions: ['.svelte', '.md'],
+	preprocess: [
+		sveltePreprocess(),
+		mdsvex({
+			extensions: ['.md'],
+			layout: {
+				writing: 'src/routes/writing/_post.svelte'
+			}
+		})
+	]
+};
 
 export default config;
